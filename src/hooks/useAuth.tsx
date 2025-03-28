bdc3fc768aceeabc35bf90b0aa44b5b2957e31c1
AviAbinash <abinashtripathy567@gmail.com>
2025-03-28 12:00:06 +0530

    last changes

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,34 +1,34 @@
-"use client";
+// "use client";
  
-import { useState, useEffect } from "react";
-import jwt from "jsonwebtoken";
+// import { useState, useEffect } from "react";
+// import jwt from "jsonwebtoken";
  
-const useAuth = () => {
-  const [authenticated, setAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
+// const useAuth = () => {
+//   const [authenticated, setAuthenticated] = useState(false);
+//   const [loading, setLoading] = useState(true);
  
-  useEffect(() => {
-    const token = localStorage.getItem("token");
+//   useEffect(() => {
+//     const token = localStorage.getItem("token");
  
-    if (!token) {
-      setAuthenticated(false);
-      setLoading(false);
-      return;
-    }
+//     if (!token) {
+//       setAuthenticated(false);
+//       setLoading(false);
+//       return;
+//     }
  
-    try {
-      jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET);
-      setAuthenticated(true);
-    } catch (error) {
-      console.error("Token verification failed:", error);
-      localStorage.removeItem("token");
-      setAuthenticated(false);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+//     try {
+//       jwt.verify(token, process.env.NEXT_PUBLIC_JWT_SECRET);
+//       // setAuthenticated(true);
+//     } catch (error) {
+//       console.error("Token verification failed:", error);
+//       localStorage.removeItem("token");
+//       setAuthenticated(false);
+//     } finally {
+//       setLoading(false);
+//     }
+//   }, []);
  
-  return { authenticated, loading };
-};
+//   return { authenticated, loading };
+// };
  
-export default useAuth;
+// export default useAuth;
